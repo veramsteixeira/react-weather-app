ef34d9fb35f4686362720b95fb322c90d5ec10ae
veramsteixeira <129678252+veramsteixeira@users.noreply.github.com>
2023-07-01 17:29:39 +0100

    Change icons size

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="d-flex align-items-center justify-content-start">
             <div className="mr-3">
-              <WeatherIcon code={props.data.icon} size={36} />
+              <WeatherIcon code={props.data.icon} size={50} />
             </div>
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
